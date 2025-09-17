d00791f61940f7db908640ca06d4e496bd3ab3f5
Woong0810 <chan081010@naver.com>
2025-09-17 19:05:32 +0900

    사각 운동, 원 운동 함수 선언

--- character_moves.py
+++ character_moves.py
@@ -6,7 +6,15 @@ open_canvas()
 grass = load_image('grass.png')
 character = load_image('character.png')
 
+def move_rectangle():
+    pass
+
+def move_circle():
+    pass
+
 while True:
+    move_rectangle()
+    move_circle()
     pass
 
 close_canvas()
